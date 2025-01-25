0af3be0a080e90c973c10367206098095e3853cb
royboot <rgrizell467@gmail.com>
2025-01-25 13:42:16 +0100

    Setup

---
+++ src/main/java/mc/tli/minigame_engine/TliMinigameEngine.java
@@ -0,0 +1,17 @@
+package mc.tli.minigame_engine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TliMinigameEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
