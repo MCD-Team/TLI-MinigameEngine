3405f8b872690c73319a4b7776e6943058c3b597
Royboot <rgrizell467@gmail.com>
2025-01-28 21:32:35 +0100

    Added utils plugin

---
+++ src/main/java/mc/tli/minigame_engine/Listeners/MenuListener.java
@@ -0,0 +1,7 @@
+package mc.tli.minigame_engine.Listeners;
+
+import org.bukkit.event.Listener;
+
+public class MenuListener implements Listener {
+    @Override
+}
--- src/main/java/mc/tli/minigame_engine/TliMinigameEngine.java
+++ src/main/java/mc/tli/minigame_engine/TliMinigameEngine.java
@@ -1,16 +1,23 @@
 package mc.tli.minigame_engine;
 
+import mc.tLIUtils.Utils;
 import mc.tli.minigame_engine.Managers.ConfigManager;
 import mc.tli.minigame_engine.instance.Testgame;
 import mc.tli.minigame_engine.moderation.banUser;
+import org.bukkit.Bukkit;
+
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TliMinigameEngine extends JavaPlugin {
 
+public final class TliMinigameEngine extends JavaPlugin {
+    public static Utils utils;
     @Override
     public void onEnable() {
+       utils = (Utils)Bukkit.getServer().getPluginManager().getPlugin("TLI-Utils");
         ConfigManager.initConfig(this);
-        getCommand("minigameban").setExecutor(new banUser(null,null));
+        getCommand("min" +
+                "igameban").setExecutor(new banUser(null,null));
     }
 
     @Override
