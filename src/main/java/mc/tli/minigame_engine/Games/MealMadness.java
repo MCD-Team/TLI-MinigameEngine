ae41c79d64c1ac5b6c4f61d50b3a3ac2601d6f1e
BeppieOostkut <117548526+BeppieOostkut@users.noreply.github.com>
2025-01-30 19:37:31 +0100

    Added all files

---
+++ src/main/java/mc/tli/minigame_engine/Games/MealMadness.java
@@ -0,0 +1,7 @@
+package mc.tli.minigame_engine.Games;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+
+public class MealMadness extends JavaPlugin implements Listener {
+}
