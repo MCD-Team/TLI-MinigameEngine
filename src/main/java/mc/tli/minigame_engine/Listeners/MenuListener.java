9ae23d8a47e38c5c36d3b92cd42b6eb7cc73eb7d
BeppieOostkut <117548526+BeppieOostkut@users.noreply.github.com>
2025-01-30 19:24:56 +0100

    Added bossbar for countdown

--- src/main/java/mc/tli/minigame_engine/Listeners/MenuListener.java
+++ src/main/java/mc/tli/minigame_engine/Listeners/MenuListener.java
@@ -1,7 +1,15 @@
 package mc.tli.minigame_engine.Listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class MenuListener implements Listener {
-    @Override
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event){
+        if(event.getItem().getItemMeta().getDisplayName().equals("Join Game")){
+            //Join game
+        }
+    }
 }
--- src/main/java/mc/tli/minigame_engine/instance/Countdown.java
+++ src/main/java/mc/tli/minigame_engine/instance/Countdown.java
@@ -3,30 +3,50 @@ package mc.tli.minigame_engine.instance;
 import mc.tli.minigame_engine.GameState;
 import mc.tli.minigame_engine.Managers.ConfigManager;
 import mc.tli.minigame_engine.TliMinigameEngine;
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
 
 public class Countdown extends BukkitRunnable {
     private TliMinigameEngine main;
     private Arena arena;
     private int countdownSeconds;
+    private BossBar bossBar;
+
     public Countdown(TliMinigameEngine main, Arena arena) {
         this.main = main;
         this.arena = arena;
         this.countdownSeconds = ConfigManager.getCountDownSeconds();
+        this.bossBar = Bukkit.createBossBar("Game starting in...", BarColor.RED, BarStyle.SOLID);
     }
+
     public void start() {
         arena.setState(GameState.COUNTINGDOWN);
+        for (UUID uuid : arena.getPlayers()) {
+            Player player = Bukkit.getPlayer(uuid);
+            if (player != null) {
+                bossBar.addPlayer(player);
+            }
+        }
         runTaskTimer(main, 0, 20);
     }
+
     @Override
     public void run() {
-        if(countdownSeconds == 0) {
+        if (countdownSeconds == 0) {
             cancel();
+            bossBar.removeAll();
             arena.startGame();
             return;
         }
-        //do something when set time is left
-//        if(countdownSeconds <= 10) {arena.sendMessage()}
+
+        bossBar.setTitle("Game starting in " + countdownSeconds + " seconds");
+        bossBar.setProgress(countdownSeconds / (double) ConfigManager.getCountDownSeconds());
         countdownSeconds--;
     }
 }
