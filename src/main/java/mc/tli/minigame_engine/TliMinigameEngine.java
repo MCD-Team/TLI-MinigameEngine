836eddf2f0a1113687f67451630cf89ed76b4243
royboot <rgrizell467@gmail.com>
2025-02-18 20:55:05 +0100

    added writing default config

--- src/main/java/mc/tli/minigame_engine/TliMinigameEngine.java
+++ src/main/java/mc/tli/minigame_engine/TliMinigameEngine.java
@@ -2,6 +2,7 @@ package mc.tli.minigame_engine;
 
 import mc.tLIUtils.Utils;
 import mc.tli.minigame_engine.commands.GameCommand;
+import mc.tli.minigame_engine.listeners.MenuListener;
 import mc.tli.minigame_engine.managers.ArenaManager;
 import mc.tli.minigame_engine.managers.ConfigManager;
 import mc.tli.minigame_engine.commands.banUser;
@@ -15,14 +16,20 @@ public final class TliMinigameEngine extends JavaPlugin {
     public void onEnable() {
         utils = (Utils)Bukkit.getServer().getPluginManager().getPlugin("TLI-Utils");
         this.arenamanger = new ArenaManager(this);
+        arenamanger.addArena();
         //initialize the config
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
         ConfigManager.initConfig(this);
         //register commands
         getCommand("moderationban").setExecutor(new banUser(this));
         getCommand("game").setExecutor(new GameCommand(this));
-
+        getServer().getPluginManager().registerEvents(new MenuListener(this), this);
     }
     public ArenaManager getArenaManager() {
         return arenamanger;
+    }
+    public Utils getUtils() {
+        return utils;
     }
 }
