bb1fdd7a15bedf967758a0c027f5345c4c498793
royboot <rgrizell467@gmail.com>
2025-02-18 21:49:28 +0100

    push

--- src/main/java/mc/tli/minigame_engine/listeners/MenuListener.java
+++ src/main/java/mc/tli/minigame_engine/listeners/MenuListener.java
@@ -1,14 +1,19 @@
 package mc.tli.minigame_engine.listeners;
 
+import mc.tLIUtils.guis;
 import mc.tli.minigame_engine.GameState;
 import mc.tli.minigame_engine.TliMinigameEngine;
 import mc.tli.minigame_engine.instance.Arena;
 import mc.tli.minigame_engine.managers.ConfigManager;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
 import java.util.List;
 
 public class MenuListener implements Listener {
@@ -18,27 +23,48 @@ public class MenuListener implements Listener {
     }
     @EventHandler
     public void onInteract(PlayerInteractEvent event){
+        if(event.getHand() == null || !event.getHand().equals(EquipmentSlot.HAND)){
+            return;
+        }
         Player player = event.getPlayer();
-        if(!(event.getItem() == null)){
-            if(event.getItem().getItemMeta().getDisplayName().equals("Join Game")){
-                List<Arena> Arenas =  plugin.getArenaManager().getArenas();
-                if(Arenas != null){
-                    if(!Arenas.isEmpty()){
-                        for(Arena arena : Arenas){
-                            //check if the arena is queueing or countdown and if the max player count hase NOT been reached then add the player
-                            if(arena.getState() == GameState.QUEUEING || arena.getState() == GameState.COUNTINGDOWN && arena.getPlayers().size() <= ConfigManager.getMaxPlayers()){
-                                arena.addPlayer(player);
-                            }else{
-                                player.sendMessage(ChatColor.RED + "An error hase occurred please try again");
-                                return;
-                            }
-                        }
-                    }else{
-                        player.sendMessage(ChatColor.RED + "No arenas found making arena try again");
-                    }
-                }
+        player.sendMessage("Open menu");
+      if(event.getHand().equals(EquipmentSlot.HAND)){
+            if(player.getInventory().getItemInMainHand() != null&& player.getInventory().getItemInMainHand().getType().equals(Material.COMPASS)){
+//                FIX THIS METHOD UTILLITIES method is broken
+//                player.openInventory(guis.TestGame(player));
             }
         }
 
+    }
+    @EventHandler
+    public void onClick(InventoryClickEvent event){
+        if(ChatColor.translateAlternateColorCodes('&', event.getView().getTitle()).equals("Join game") && event.getCurrentItem() != null){
+            event.setCancelled(true);
+            Player player = (Player) event.getWhoClicked();
+            switch(event.getRawSlot()){
+                case 22:
+                    List<Arena> Arenas =  plugin.getArenaManager().getArenas();
+                    if(Arenas != null){
+                        if(!Arenas.isEmpty()){
+                            for(Arena arena : Arenas){
+                                //check if the arena is queueing or countdown and if the max player count hase NOT been reached then add the player
+                                if(arena.getState() == GameState.QUEUEING || arena.getState() == GameState.COUNTINGDOWN && arena.getPlayers().size() <= ConfigManager.getMaxPlayers()){
+                                    arena.addPlayer(player);
+                                }else{
+                                    player.sendMessage(ChatColor.RED + "An error hase occurred please try again");
+                                    return;
+                                }
+                            }
+                        }else{
+                            player.sendMessage(ChatColor.RED + "No arenas found making arena try again");
+                        }
+                    }
+                    break;
+                default:
+                    player.closeInventory();
+                    break;
+            }
+            player.closeInventory();
+        }
     }
 }
