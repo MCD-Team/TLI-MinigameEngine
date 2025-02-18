cab529c2ee811c1a39c6a76a03c3a393b4e0e3a3
royboot <rgrizell467@gmail.com>
2025-02-18 20:54:54 +0100

    added debug messages

--- src/main/java/mc/tli/minigame_engine/commands/GameCommand.java
+++ src/main/java/mc/tli/minigame_engine/commands/GameCommand.java
@@ -2,6 +2,7 @@ package mc.tli.minigame_engine.commands;
 
 import mc.tli.minigame_engine.TliMinigameEngine;
 import mc.tli.minigame_engine.instance.Arena;
+import mc.tli.minigame_engine.managers.ArenaManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,6 +19,14 @@ public class GameCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if(commandSender instanceof Player player) {
             Arena arena = plugin.getArenaManager().getArena(player);
+            commandSender.sendMessage("DEBUG: Arena is " + (arena == null ? "null" : "not null"));
+            if(arena == null) {
+               plugin.getArenaManager().addArena();
+               commandSender.sendMessage(ChatColor.GREEN + "Arena added to the game!");
+               arena = plugin.getArenaManager().getArena(player);
+               commandSender.sendMessage("DEBUG: Arena is " + (arena == null ? "null" : "not null"));
+            }
+
             //check if there is 1 argument given and if the string given contains leave then remove the player from the arena
             if (args.length == 1 && args[0].equalsIgnoreCase("leave")) {
                 if (arena != null) {
@@ -38,7 +47,8 @@ public class GameCommand implements CommandExecutor {
                     player.sendMessage(ChatColor.GREEN + "-" + ar.getId());
                 }
                 return false;
-            } else {
+
+            }else {
                 player.sendMessage(ChatColor.RED + "Usage: /game leave");
             }
         }
